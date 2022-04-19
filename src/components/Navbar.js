2c2250e974d2cc32273ffc8828fa53462f3223a4
vivekanand93 <78248575+vivekanand93@users.noreply.github.com>
2022-04-19 10:37:52 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,52 +20,110 @@ export default function Navbar(props) {
         <li className="nav-item">
           <a  className="nav-link" href='/about'>{props.aboutText}</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Tools
-          </a>
-          <ul className="dropdown-menu" aria-label led by="navbarDropdown">
-            <li><a className="dropdown-item" href="/">Action</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="/">Disabled</a>
-        </li>
+                  <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="/"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Customize Themes
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    {" "}
+                    <div
+                      className={`dropdown-item form-check form-switch text-${
+                        props.mode === "light" ? "dark" : "light"
+                      } mx-4`}
+                      style={{ width: "inherit" }}
+                    >
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexSwitchCheckDefault"
+                      >
+                        Change theme
+                      </label>
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        onClick={() => {
+                          props.toggleMode(null);
+                        }}
+                        role="switch"
+                        aria-checked="true"
+                        id="flexSwitchCheckDefault"
+                      />
+                    </div>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    {" "}
+                    <div
+                      className="dropdown-item bg-primary rounded mx-1 my-1"
+                      onClick={() => {
+                        props.toggleMode("primary");
+                      }}
+                      style={{
+                        height: "20px",
+                        width: "inherit",
+                        cursor: "pointer",
+                      }}
+                    ></div>
+                  </li>
+                  <li>
+                    {" "}
+                    <div
+                      className="dropdown-item bg-danger rounded mx-1 my-1"
+                      onClick={() => {
+                        props.toggleMode("danger");
+                      }}
+                      style={{
+                        height: "20px",
+                        width: "inherit",
+                        cursor: "pointer",
+                      }}
+                    ></div>
+                  </li>
+                  <li>
+                    <div
+                      className="dropdown-item bg-warning rounded mx-1 my-1"
+                      onClick={() => {
+                        props.toggleMode("warning");
+                      }}
+                      style={{
+                        height: "20px",
+                        width: "inherit",
+                        cursor: "pointer",
+                      }}
+                    ></div>
+                  </li>
+                  <li>
+                    {" "}
+                    <div
+                      className="dropdown-item bg-info rounded mx-1 my-1"
+                      onClick={() => {
+                        props.toggleMode("info");
+                      }}
+                      style={{
+                        height: "20px",
+                        width: "inherit",
+                        cursor: "pointer",
+                      }}
+                    ></div>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="/">
+                  Thank you for your visit
+                </a>
+              </li>
       </ul>
-      <div className="spinner-border text-primary"  role="status">
-  <span className="visually-hidden">Loading...</span>
-    </div>
- {/*    <div className="spinner-border text-secondary"  role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-    <div className="spinner-border text-success"  role="status"> */}
-      {/* <span className="visually-hidden">Loading...</span>
-    </div> */}
-    <div className="spinner-border text-danger"  role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-    <div className="spinner-border text-warning"  role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-    <div className="spinner-border text-info"  role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-    {/* <div className="spinner-border text-light" role="status">
-      <span className="visually-hidden">Loading...</span>
-    </div>
-    <div className="spinner-border text-dark" role="status">
-      <span className="visually-hidden">Loading...</span>
-</div> */}
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-        <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'} mx-3`}>
-         <input className="form-check-input" type="checkbox" onClick={props.toggleMode} role="switch" id="flexSwitchCheckDefault"/>
-         <label className="form-check-label" htmlFor="flexSwitchCheckDefault">toggle mode</label>
         </div>
     </div>
   </div>
