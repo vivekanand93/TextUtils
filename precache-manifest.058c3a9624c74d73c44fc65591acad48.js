169911b03ea55008080d5d7ae1bf768d5cc2a04f
vivekanand93 <78248575+vivekanand93@users.noreply.github.com>
2022-04-20 11:53:13 +0530

    Updates

---
+++ precache-manifest.058c3a9624c74d73c44fc65591acad48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "476bf48b9825ee34c8e2",
+    "url": "/TextUtils/static/css/main.251bc4a1.chunk.css"
+  },
+  {
+    "revision": "476bf48b9825ee34c8e2",
+    "url": "/TextUtils/static/js/main.1a03dddc.chunk.js"
+  },
+  {
+    "revision": "7538bc085fe8f2993a98",
+    "url": "/TextUtils/static/js/runtime~main.a7d0d9dc.js"
+  },
+  {
+    "revision": "d752564782eaa787c2c1",
+    "url": "/TextUtils/static/js/2.97a2e95c.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/TextUtils/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "1511b422cd44e36c8b9b353e7b85b12a",
+    "url": "/TextUtils/index.html"
+  }
+];
